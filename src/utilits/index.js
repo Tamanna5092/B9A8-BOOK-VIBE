ed3c77d85191972e570d3e4622bdaf1cae982b43
Tamanna@5092 <tamannaaktermoni51@gmailcom>
2024-08-05 20:43:27 -1200

    local storage

--- src/utilits/index.js
+++ src/utilits/index.js
@@ -1,37 +1,38 @@
-import toast from 'react-hot-toast';
+import toast from "react-hot-toast";
 
-export const getBooks = () =>{
-    let books = []
-    const storedBooks = localStorage.getItem('books')
-    if(storedBooks){
-        books = JSON.parse(storedBooks)
-    }
-    return books
-}
+export const getBooks = () => {
+  let books = [];
+  const storedBooks = localStorage.getItem("books");
+  if (storedBooks) {
+    books = JSON.parse(storedBooks);
+  }
+  return books;
+};
 
 // read book
-export const saveReadBook = book => {
-    let books = getBooks()
-    const isExist = books.find(b => b.bookId === book.bookId)
-    if(isExist){
-        return toast.error('Already Read!')
-    }
-    else{
-        books.push(book)
-    localStorage.setItem('books', JSON.stringify(books))
-    toast.success('Book successfully added!')
-    }
-}
-
+export const saveReadBook = (book) => {
+  let books = getBooks();
+  const isExist = books.find((b) => b.bookId === book.bookId);
+  if (isExist) {
+    return toast.error("Already Read!");
+  }
+  else {
+    books.push(book);
+    localStorage.setItem("books", JSON.stringify(books));
+    toast.success("Book successfully added!");
+  }
+};
 
 // wishlist
-export const saveWishlist = book => {
-    let books = getBooks()
-    const isExist = books.find(b => b.bookId === book.bookId)
-    if(isExist){
-        return toast.error('Already Wishlisted!')
-    }
-    books.push(book)
-    localStorage.setItem('books', JSON.stringify(books))
-    toast.success('Book successfully Wishlisted!')
-}
+export const saveWishlist = (book) => {
+  let books = getBooks();
+  const isExist = books.find((b) => b.bookId === book.bookId);
+  if (isExist) {
+    return toast.error("Already Wishlisted!");
+  }
+  else {
+    books.push(book);
+    localStorage.setItem("books", JSON.stringify(books));
+    toast.success("Book successfully Wishlisted!");
+  }
+};
